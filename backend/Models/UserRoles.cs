43d3d955c02a66d4eba8f22f410a3151e9450c29
JOAO VICTOR GOMES PRADO <uniejprado@fei.edu.br>
2026-01-27 10:46:01 -0300

    Etapa 2 finalizada

--- backend/Models/UserRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EduConnect.Api.Models;
-
-public static class UserRoles
-{
-    public const string Aluno = "Aluno";
-    public const string Professor = "Professor";
-    public const string Administrador = "Administrador";
-}
-
--- frontend/src/auth/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "./AuthContext";
-
-export default function ProtectedRoute({ children }) {
-  const { user } = useAuth();
-  if (!user?.token) return <Navigate to="/" replace />;
-  return children;
-}
